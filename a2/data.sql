24197bbd226e2feffb8e90438b18a6e84c2beb4c
Connor Smith <connorjsmith101@gmail.com>
2017-02-28 15:16:50 -0500

    Added q1 and setup files

---
+++ a2/data.sql
@@ -0,0 +1,67 @@
+-- If there is already any data in these tables, empty it out.
+
+TRUNCATE TABLE Result CASCADE;
+TRUNCATE TABLE Grade CASCADE;
+TRUNCATE TABLE RubricItem CASCADE;
+TRUNCATE TABLE Grader CASCADE;
+TRUNCATE TABLE Submissions CASCADE;
+TRUNCATE TABLE Membership CASCADE;
+TRUNCATE TABLE AssignmentGroup CASCADE;
+TRUNCATE TABLE Required CASCADE;
+TRUNCATE TABLE Assignment CASCADE;
+TRUNCATE TABLE MarkusUser CASCADE;
+
+
+-- Now insert data from scratch.
+
+INSERT INTO MarkusUser VALUES ('i1', 'iln1', 'ifn1', 'instructor');
+INSERT INTO MarkusUser VALUES ('s1', 'sln1', 'sfn1', 'student');
+INSERT INTO MarkusUser VALUES ('s2', 'sln2', 'sfn2', 'student');
+INSERT INTO MarkusUser VALUES ('s3', 'sln3', 'sfn3', 'student');
+INSERT INTO MarkusUser VALUES ('s4', 'sln4', 'sfn4', 'student');
+INSERT INTO MarkusUser VALUES ('t1', 'tln1', 'tfn1', 'TA');
+
+INSERT INTO Assignment VALUES (1000, 'a1', '2017-02-08 20:00', 1, 2);
+INSERT INTO Assignment VALUES (0, 'orphan_assignment', '2017-02-08 20:00', 1, 2);
+
+INSERT INTO Required VALUES (1000, 'A1.pdf');
+
+INSERT INTO AssignmentGroup VALUES (2000, 1000, 'repo_url');
+
+INSERT INTO Membership VALUES ('s1', 2000);
+INSERT INTO Membership VALUES ('s2', 2000);
+
+INSERT INTO Submissions VALUES (3000, 'A1.pdf', 's1', 2000, '2017-02-08 19:59');
+
+INSERT INTO Grader VALUES (2000, 't1');
+
+INSERT INTO RubricItem VALUES (4000, 1000, 'style', 4, 0.25);
+INSERT INTO RubricItem VALUES (4001, 1000, 'tester', 12, 0.75);
+
+INSERT INTO Grade VALUES (2000, 4000, 3);
+INSERT INTO Grade VALUES (2000, 4001, 9);
+
+INSERT INTO Result VALUES (2000, 7.5, true);
+
+
+-- Begin my own custom test cases
+-- Add an Assignment with no grades associated with it
+INSERT INTO Assignment VALUES (1, 'ungraded_assignment', '2017-02-09 20:00', 2, 2);
+INSERT INTO AssignmentGroup VALUES (2002, 1, 'repo_url1');
+INSERT INTO RubricItem VALUES (4004, 1, 'style-ungraded', 5, 0.75); -- Weighted Divisor of 39.75
+INSERT INTO RubricItem VALUES (4005, 1, 'tester-ungraded', 50, 0.75); -- """"
+INSERT INTO Membership VALUES ('s3', 2002);
+INSERT INTO Membership VALUES ('s4', 2002);
+
+-- Add another Assignment, AssignmentGroup, members, RubricItems, and Grades
+INSERT INTO Assignment VALUES (1001, 'a1.1', '2017-02-09 20:00', 1, 2);
+INSERT INTO AssignmentGroup VALUES (2001, 1001, 'repo_url1');
+INSERT INTO Membership VALUES ('s1', 2001);
+INSERT INTO Membership VALUES ('s2', 2001);
+-- Also test that this works for total weights > 1.0
+INSERT INTO RubricItem VALUES (4002, 1001, 'style-a1.1', 5, 0.75); -- Weighted Divisor of 39.75
+INSERT INTO RubricItem VALUES (4003, 1001, 'tester-a1.1', 50, 0.75); -- """"
+INSERT INTO Grade VALUES (2001, 4002, 3);
+INSERT INTO Grade VALUES (2001, 4003, 20);
+INSERT INTO Result VALUES (2001, 17.25, false); -- 3*0.75 + 20*0.75
+
---
+++ a2/q1.sql
@@ -0,0 +1,69 @@
+-- Create a table of the weighted items
+-- IE. for a rubric with assignment_id=123,group_id=456 sections weighted 2/20% and 4/80%, and grades 1 and 4,
+-- output (assignment_id, group_id, section_grade) of {(123, 456, 1/2 * 20), (123, 456, 4/4*80)}
+-- A groups mark for an assignment is the group-by (assignment_id, group_id) and sum(section_grade)
+
+-- Reset all views
+DROP VIEW CountedBucketStudentPercentages;
+DROP VIEW BucketedStudentPercentages;
+DROP VIEW StudentPercentageGrade;
+DROP VIEW GroupAssignmentPercentageGrade;
+DROP VIEW AssigmentDivisors;
+
+-- Define views for intermediate results
+CREATE VIEW AssignmentDivisors AS (
+    SELECT Assignment.assignment_id, SUM(partial_divisor) AS weighted_divisor
+    FROM Assignment LEFT JOIN (
+        SELECT assignment_id, rubric_id, (out_of * weight) as partial_divisor
+        FROM RubricItem
+    ) AS IntermediateResult on Assignment.assignment_id = IntermediateResult.assignment_id
+    GROUP BY Assignment.assignment_id
+);
+
+-- Pad with nulls for Assignments without groups or without results for any group
+CREATE VIEW GroupAssignmentPercentageGrade AS (
+    SELECT ad.assignment_id, ag.group_id, (mark * 100/ weighted_divisor) as percentage
+    FROM AssignmentDivisors ad
+        LEFT JOIN AssignmentGroup ag
+            ON ad.assignment_id = ag.assignment_id
+        LEFT JOIN Result r
+            ON ag.group_id = r.group_id
+);
+
+-- Pad with nulls for Assignments without members
+CREATE VIEW StudentPercentageGrade AS (
+    SELECT assignment_id, username, percentage
+    FROM GroupAssignmentPercentageGrade g LEFT JOIN Membership m 
+        ON g.group_id = m.group_id
+);
+
+CREATE VIEW BucketedStudentPercentages AS (
+    SELECT assignment_id,
+           username,
+           percentage,
+           (CASE WHEN (percentage >= 80 AND percentage <= 100) THEN 1 ELSE 0 END) as bool80_100,
+           (CASE WHEN (percentage >= 60 AND percentage <= 79) THEN 1 ELSE 0 END) as bool60_79,
+           (CASE WHEN (percentage >= 50 AND percentage <= 59) THEN 1 ELSE 0 END) as bool50_59,
+           (CASE WHEN (percentage < 50) THEN 1 ELSE 0 END) as bool_50
+    FROM StudentPercentageGrade
+);
+
+CREATE VIEW CountedBucketStudentPercentages AS (
+    SELECT assignment_id, 
+           avg(percentage) as average_mark_percentage, 
+           sum(bool80_100) as num_80_100,
+           sum(bool60_79) as num_60_79,
+           sum(bool50_59) as num_50_59, 
+           sum(bool_50) as num_50
+    FROM BucketedStudentPercentages
+    GROUP BY assignment_id
+
+);    
+
+-- TODO: remove these "print statements"
+SELECT * FROM AssignmentDivisors;
+SELECT * FROM GroupAssignmentPercentageGrade;
+SELECT * FROM StudentPercentageGrade;
+SELECT * FROM BucketedStudentPercentages;
+SELECT * FROM CountedBucketStudentPercentages;
+
