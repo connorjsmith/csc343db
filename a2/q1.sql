6e8e90dce77ecd083851843c7be99257442ccdf4
Connor Smith <connorjsmith101@gmail.com>
2017-03-06 15:17:03 -0500

    Added mostly complete A2-Q1 answer

--- a2/q1.sql
+++ a2/q1.sql
@@ -3,12 +3,28 @@
 -- output (assignment_id, group_id, section_grade) of {(123, 456, 1/2 * 20), (123, 456, 4/4*80)}
 -- A groups mark for an assignment is the group-by (assignment_id, group_id) and sum(section_grade)
 
+SET SEARCH_PATH TO markus;
+DROP TABLE IF EXISTS q1;
+
+-- You must not change this table definition.
+CREATE TABLE q1 (
+	assignment_id integer,
+	average_mark_percent real,
+	num_80_100 integer,
+	num_60_79 integer,
+	num_50_59 integer,
+	num_0_49 integer
+);
+
+-- You may find it convenient to do this for each of the views
+-- that define your intermediate steps.  (But give them better names!)
+
 -- Reset all views
 DROP VIEW IF EXISTS CountedBucketStudentPercentages;
 DROP VIEW IF EXISTS BucketedStudentPercentages;
 DROP VIEW IF EXISTS StudentPercentageGrade;
 DROP VIEW IF EXISTS GroupAssignmentPercentageGrade;
-DROP VIEW IF EXISTS AssigmentDivisors;
+DROP VIEW IF EXISTS AssignmentDivisors;
 
 -- Define views for intermediate results
 CREATE VIEW AssignmentDivisors AS (
@@ -42,8 +58,8 @@ CREATE VIEW BucketedStudentPercentages AS (
            username,
            percentage,
            (CASE WHEN (percentage >= 80 AND percentage <= 100) THEN 1 ELSE 0 END) as bool80_100,
-           (CASE WHEN (percentage >= 60 AND percentage <= 79) THEN 1 ELSE 0 END) as bool60_79,
-           (CASE WHEN (percentage >= 50 AND percentage <= 59) THEN 1 ELSE 0 END) as bool50_59,
+           (CASE WHEN (percentage >= 60 AND percentage < 80) THEN 1 ELSE 0 END) as bool60_79,
+           (CASE WHEN (percentage >= 50 AND percentage < 60) THEN 1 ELSE 0 END) as bool50_59,
            (CASE WHEN (percentage < 50) THEN 1 ELSE 0 END) as bool_50
     FROM StudentPercentageGrade
 );
@@ -53,17 +69,13 @@ CREATE VIEW CountedBucketStudentPercentages AS (
            avg(percentage) as average_mark_percentage, 
            sum(bool80_100) as num_80_100,
            sum(bool60_79) as num_60_79,
-           sum(bool50_59) as num_50_59, 
-           sum(bool_50) as num_50
+           sum(bool50_59) as num_50_59,
+           sum(bool_50) as num_0_49
     FROM BucketedStudentPercentages
     GROUP BY assignment_id
 
 );    
 
--- TODO: remove these "print statements"
--- SELECT * FROM AssignmentDivisors;
--- SELECT * FROM GroupAssignmentPercentageGrade;
--- SELECT * FROM StudentPercentageGrade;
--- SELECT * FROM BucketedStudentPercentages;
-SELECT * FROM CountedBucketStudentPercentages;
+INSERT INTO q1 (SELECT * FROM CountedBucketStudentPercentages);
+SELECT * FROM q1;
 
