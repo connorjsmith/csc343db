0101d94ed0d1d3c0b970cbaea60cc5d10a69ced6
Connor Smith <connorjsmith101@gmail.com>
2017-03-06 22:47:46 -0500

    Added plan for q5

--- a2/q5.sql
+++ a2/q5.sql
@@ -19,3 +19,10 @@ DROP VIEW IF EXISTS intermediate_step CASCADE;
 -- Final answer.
 INSERT INTO q5 
 	-- put a final query here so that its results will go into the table.
+
+
+-- Associate assignment_id, (nullable) group_id, (nullable) grader_username for every group_id
+-- Group By assignment_id, grader_username. COUNT(DISTINCT group_id) as num_assigned
+-- Filter above by assignment_id, GROUP BY grader_username, assignment_id. HAVING MAX(num_assigned) - MIN(num_assigned) >= 10
+
+-- Null assignments (no graders, no groups, etc.) should no be reported (TODO: i think?)
