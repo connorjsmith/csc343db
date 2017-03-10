730adafdb8827b20644459d54f6fbc138baea4c9
Connor Smith <connorjsmith101@gmail.com>
2017-03-09 21:22:53 -0500

    Added q6 solution with TODOs

--- a2/q6.sql
+++ a2/q6.sql
@@ -17,23 +17,78 @@ CREATE TABLE q6 (
 
 -- You may find it convenient to do this for each of the views
 -- that define your intermediate steps.  (But give them better names!)
-DROP VIEW IF EXISTS intermediate_step CASCADE;
+DROP VIEW IF EXISTS AssignmentGroupSubmissions CASCADE;
+DROP VIEW IF EXISTS first_submissions_per_group CASCADE;
+DROP VIEW IF EXISTS last_submissions_per_group CASCADE;
 
 -- Define views for your intermediate steps here.
+-- JOIN to get assignment_id | group_id | submission_date for every submission by a group on that assignment
+-- Filter to find assignment_id with description = 'A1'
+CREATE VIEW AssignmentGroupSubmissions AS (
+    SELECT ag.assignment_id, ag.group_id, s.file_name, s.username, s.submission_date
+    FROM AssignmentGroup ag
+        JOIN Assignment a
+            ON ag.assignment_id = a.assignment_id
+        LEFT JOIN Submissions s -- might not have a submission
+            ON ag.group_id = s.group_id
+    WHERE a.description = 'A1'
+);
+
+-- CREATE VIEW first_submissions_per_group AS (assignment_id, group_id, MIN(submission_date))
+CREATE VIEW first_submissions_per_group AS (
+    SELECT assignment_id,
+           group_id,
+           file_name AS first_file,
+           submission_date AS first_time,
+           username AS first_submitter
+    FROM AssignmentGroupSubmissions outer_table
+    WHERE submission_date = (
+        SELECT MIN(submission_date)
+        FROM AssignmentGroupSubmissions
+        WHERE outer_table.assignment_id = assignment_id
+            AND outer_table.group_id = group_id
+    ) OR submission_date IS NULL -- include groups with no submissions
+);
+
+-- CREATE VIEW first_submissions_per_group AS (assignment_id, group_id, MIN(submission_date))
+CREATE VIEW last_submissions_per_group AS (
+    SELECT outer_table.assignment_id,
+           group_id,
+           file_name AS last_file,
+           submission_date AS last_time,
+           username AS last_submitter
+    FROM AssignmentGroupSubmissions outer_table
+    WHERE submission_date = (
+        SELECT MAX(submission_date)
+        FROM AssignmentGroupSubmissions
+        WHERE outer_table.assignment_id = assignment_id
+            AND outer_table.group_id = group_id
+    ) OR submission_date IS NULL -- include groups with no submissions
+);
+
+-- Cross product these two tables, joined on assignment_id and group_id
+CREATE VIEW first_last_submissions_per_group AS (
+    SELECT first.group_id,
+           first_file,
+           first_time,
+           first_submitter,
+           last_file,
+           last_time,
+           last_submitter,
+           last_time - first_time AS elapsed_time
+    FROM first_submissions_per_group first JOIN last_submissions_per_group last
+        ON first.group_id = last.group_id
+);
+-- SELECT * FROM AssignmentGroupSubmissions; -- TODO remove
+-- SELECT * FROM first_submissions_per_group; -- TODO remove
+-- SELECT * FROM last_submissions_per_group; -- TODO remove
+
 
 -- Final answer.
-INSERT INTO q6
+INSERT INTO q6 (SELECT * FROM first_last_submissions_per_group);
+-- SELECT * FROM q6; -- TODO remove
     -- put a final query here so that its results will go into the table.
 
 
 
 
--- Filter to find assignment_id with description = 'A1'
--- JOIN to get assignment_id | group_id | submission_date for every submission by a group on that assignment
--- CREATE VIEW first_submissions_per_group AS (assignment_id, group_id, MIN(submission_date))
--- CREATE VIEW last_submissions_per_group AS (assignment_id, group_id, MAX(submission_date))
--- Cross product these two tables, joined on assignment_id and group_id
-
-
-
--- Get other info about these submissions along the way, as per the schema 
