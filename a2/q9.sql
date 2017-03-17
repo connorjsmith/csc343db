2a7ddc4acd3e3314fd7c2cd51a7fbacc1d05463c
Connor Smith <connorjsmith101@gmail.com>
2017-03-17 15:54:30 -0400

    Finished q9

--- a2/q9.sql
+++ a2/q9.sql
@@ -31,7 +31,7 @@ CREATE VIEW AllStudentUsernames AS (
 CREATE VIEW AllStudentPairs AS (
     SELECT s1.username AS s1_username, s2.username as s2_username
     FROM AllStudentUsernames s1 CROSS JOIN AllStudentUsernames s2
-	WHERE s1.username <> s2.username
+    WHERE s1.username <> s2.username
 );
 
 -- List all group_ids for assignments that had max_group_size >= 2
@@ -59,32 +59,30 @@ CREATE VIEW SeparablePairs AS (
     SELECT s1_username, s2_username
     FROM StudentPairsPairableGroups sppg
     WHERE NOT EXISTS (SELECT m.group_id -- s1 and s2 both belong to that group
-                  FROM Membership m
-		          WHERE m.username = sppg.s2_username
-	                  AND m.group_id = sppg.group_id)
+                      FROM Membership m
+                      WHERE m.username = sppg.s2_username
+                          AND m.group_id = sppg.group_id)
 );
 
 -- For every group that s1_username was in, ensure that s2_username was also in that group. If not, remove that pair
 CREATE VIEW InseparablePairs AS (
-	SELECT s1_username, s2_username FROM AllStudentPairs
-	EXCEPT
-	SELECT s1_username, s2_username FROM SeparablePairs -- Remove (s1,s2)
-	EXCEPT
-	SELECT s2_username, s1_username FROM SeparablePairs -- Remove (s2,s1), relation must be symmetric
+    SELECT s1_username, s2_username FROM AllStudentPairs
+    EXCEPT
+    SELECT s1_username, s2_username FROM SeparablePairs -- Remove (s1,s2)
+    EXCEPT
+    SELECT s2_username, s1_username FROM SeparablePairs -- Remove (s2,s1), relation must be symmetric
 );
 
 -- Keep only the pairs where s1_username < s2_username alphabetically
 CREATE VIEW DedupedInseparablePairs AS (
-	SELECT s1_username, s2_username
-	FROM InseparablePairs
-	WHERE s1_username < s2_username
+    SELECT s1_username, s2_username
+    FROM InseparablePairs
+    WHERE s1_username < s2_username
 );
 
-SELECT * FROM DedupedInseparablePairs;
-	
 -- Final answer.
 INSERT INTO q9 (
-	SELECT s1_username AS student1,
-		   s2_username AS student2
-	FROM DedupedInseparablePairs
+    SELECT s1_username AS student1,
+           s2_username AS student2
+    FROM DedupedInseparablePairs
 );
